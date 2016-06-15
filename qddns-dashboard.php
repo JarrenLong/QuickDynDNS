619b13626702816e6f43d9f2f57ffb8d259c2fc5
Jarren Long <jarrenlong@gmail.com>
2016-06-15 12:08:41 -0700

    Starting to lay out dashboard widget

--- qddns-dashboard.php
+++ qddns-dashboard.php
@@ -13,17 +13,50 @@ if(is_admin()) {
 		if( !$admin_hostname || $admin_hostname == $admin_ip_address )
 			$admin_hostname = 'Unknown';
 		
+		$num_users = 0;
+		$total_requests = 0;
+		
 		// display information
 		echo '<div style="display:table; width: 100%;">';
-		echo '<div style="display:table-cell;"><big><strong>' . $admin_ip_address . '</strong></big></div>';
+		echo '<div style="display:table-cell;"><big><strong>Your IP is ' . $admin_ip_address . '</strong></big></div>';
 		if($admin_hostname != 'unknown')
 			echo '<div style="display:table-cell; text-align: right;"><small>(' . __('hostname', 'qddns-address') . ' : ' . $admin_hostname . ')</small></div>';
 		echo "</div>\n\n";
-		echo '<div class="box-ip"><hr> Your IP address is something you might rarely think about, it\'s important to know what your IP address is and when it changes. Your IP address is used to identify computers on the Internet. <hr> Want to know more about IP addresses <a href="https://en.wikipedia.org/wiki/IP_address">click here</a> - Wiki Reference.  <hr>Show IP address, version 1.3</div>';
+		echo '
+		<div class="box-ip">
+			<hr>
+			This month, ' . $num_users . ' users have made ' . $total_requests . ' IP Lookup requests:
+			<table class="wp-list-table widefat fixed striped posts">
+				<thead>
+				<tr>
+					<td>Source</td>
+					<td>Requests</td>
+				</tr>
+				</thead>
+				<tr>
+					<td>Install</td>
+					<td>0</td>
+				</tr>
+				<tr>
+					<td>Shortcode</td>
+					<td>0</td>
+				</tr>
+				<tr>
+					<td>Widget</td>
+					<td>0</td>
+				</tr>
+				<tr>
+					<td>Service</td>
+					<td>0</td>
+				</tr>
+			</table>
+			<hr>
+			Like this plugin? <a href="http://jlong.co/donate">Make a donation</a> to keep it going!
+		</div>';
 	}
 
 	function qddns_add_dashboard_widgets() {
-		wp_add_dashboard_widget('qddns_dashboard_widget', __('Your IP Address & Hostname', 'qddns-address'), 'qddns_dashboard_widget_function');
+		wp_add_dashboard_widget('qddns_dashboard_widget', __('Quick DynDNS Stats', 'qddns-address'), 'qddns_dashboard_widget_function');
 	}
 
 	add_action('wp_dashboard_setup', 'qddns_add_dashboard_widgets' );
