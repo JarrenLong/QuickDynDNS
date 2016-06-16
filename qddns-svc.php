4755adaa2e98178140c7efa9fde3e2c381f648b9
Jarren Long <jarrenlong@gmail.com>
2016-06-16 10:14:50 -0700

    Service now returns IP as valid JSON string

--- qddns-svc.php
+++ qddns-svc.php
@@ -1,9 +1,9 @@
 <?php
 
-header('Content-Type: text/plain;charset=utf-8');
+header('Content-Type: application/json;charset=utf-8');
 
 if( current_user_has_auth() ) {
-	print '{ "ip" : "' . get_client_ip('service') . '" }';
+	wp_send_json( array( 'ip' => get_client_ip( 'service' ) ) );
 } else {
 	// TODO: Redirect to error page
 	print '401 Not Authorized';
