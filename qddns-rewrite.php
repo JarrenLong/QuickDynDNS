d4920f560918c7338d8e0b2868f99c527a51c647
Jarren Long <jarrenlong@gmail.com>
2016-06-21 15:55:35 -0700

    Cleaning up rewrite logic

--- qddns-rewrite.php
+++ qddns-rewrite.php
@@ -4,31 +4,29 @@ function qddns_rewrite_activate() {
 	qddns_rewrite_init();
 	flush_rewrite_rules();
 }
-register_activation_hook(plugin_name(), 'qddns_rewrite_activate');
+register_activation_hook( plugin_name(), 'qddns_rewrite_activate' );
 
 function qddns_rewrite_deactivate() {
 	flush_rewrite_rules();
 }
-register_deactivation_hook(plugin_name(), 'qddns_rewrite_deactivate'); 
+register_deactivation_hook( plugin_name(), 'qddns_rewrite_deactivate' ); 
 
 function qddns_rewrite_init() {
-	add_rewrite_rule('dns\??([^/]*)', 'index.php?pagename=qddns&$matches[1]', 'top');
+	add_rewrite_rule( 'dns\??([^/]*)', 'index.php?pagename=qddns&$matches[1]', 'top' );
 }
 add_action( 'init', 'qddns_rewrite_init' );
 
 function qddns_rewrite_query_vars( $query_vars ) {
-	array_push($query_vars, 'qdf', 'qda');
+	array_push( $query_vars, 'qdf', 'qda' );
 	
     return $query_vars;
 }
 add_filter( 'query_vars', 'qddns_rewrite_query_vars' );
 
-function qddns_custom_page_display() {
-	$page = get_query_var('pagename');
-	$auth = get_query_var('qda');
-	$fmt = get_query_var('qdf');
+function qddns_rewrite_display_custom_page() {
+	$page = get_query_var( 'pagename' );
 	
-	if ('qddns' == $page) {
+	if ( 'qddns' == $page ) {
 		$auth = get_query_var('qda');
 		$fmt = get_query_var('qdf');
 		
@@ -38,7 +36,7 @@ function qddns_custom_page_display() {
 			
 			if( $fmt == 'json' ) {
 				// Send response as JSON
-				header('Content-Type: application/json;charset=utf-8');
+				header( 'Content-Type: application/json;charset=utf-8' );
 				
 				wp_send_json( array(
 					'QDDNS' => array(
@@ -57,7 +55,7 @@ function qddns_custom_page_display() {
 </QDDNS>';
 			} else {
 				// Send response as Text
-				header('Content-Type: text/plain;charset=utf-8');
+				header( 'Content-Type: text/plain;charset=utf-8' );
 
 				print $cur_ip;
 			}
@@ -68,20 +66,5 @@ function qddns_custom_page_display() {
 		exit();
 	}
 }
-add_filter('template_redirect', 'qddns_custom_page_display');
- 
-function qddns_rewrite_parse_request( $wp ) {
-	if ( array_key_exists( 'name', $wp->query_vars ) ) {
-		$k = $wp->query_vars['name'];
-		
-		if (strpos($k, 'qddns') !== false) {
-			print var_dump($wp->query_vars);
-			
-			include 'qddns-svc.php';
-			exit();
-		}
-    }
-    return;
-}
-//add_action( 'parse_request', 'qddns_rewrite_parse_request' );
+add_filter( 'template_redirect', 'qddns_rewrite_display_custom_page' );
 ?>
