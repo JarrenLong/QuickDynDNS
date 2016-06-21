12df1ba77771195adcf4b04cb6eb7f0ca9ddffc6
Jarren Long <jarrenlong@gmail.com>
2016-06-21 14:28:07 -0700

    Changed admin text for widget config for clarity

--- qddns-widget.php
+++ qddns-widget.php
@@ -39,7 +39,7 @@ class qddns_widget extends WP_Widget {
 		// Widget admin form
 ?>
 <p>
-<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Text Before IP:' ); ?></label>
 <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 </p>
 <?php 
